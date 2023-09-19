05c4095738326fefa6fe1dfdb91a4cd8b58a89e1
RAndre <andr.raphael@yahoo.fr>
2023-09-19 16:35:08 +0200

    feat : crud user

---
+++ src/Form/UserType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nickname')
+            ->add('email')
+            ->add('roles', ChoiceType::class, [
+                'label' => 'Rôles',
+                'choices' => [
+                    'Administrateur' => 'ROLE_ADMIN',
+                ],
+                'multiple' => true, // Permet de sélectionner plusieurs rôles
+                'expanded' => true, // Affiche les rôles en tant que cases à cocher
+            ]);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
